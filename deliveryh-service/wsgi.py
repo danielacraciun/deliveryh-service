aed6980651969d9df36efcb91f16f5004ecf5407
Dana <craciundaniela19@gmail.com>
2018-01-22 22:01:52 +0200

    Modified program to import get_wsgi_application from correct module

--- deliveryh-service/wsgi.py
+++ deliveryh-service/wsgi.py
@@ -1,13 +1,6 @@
-"""
-WSGI config for viral project.
-It exposes the WSGI callable as a module-level variable named ``application``.
-For more information on this file, see
-https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/gunicorn/
-"""
 import os
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "deliveryh-service.config")
-os.environ.setdefault("DJANGO_CONFIGURATION", "Production")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "deliveryh-service.settings")
 
-from configurations.wsgi import get_wsgi_application  # noqa
+from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
