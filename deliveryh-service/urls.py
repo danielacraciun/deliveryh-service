3d5b86126ba7a7ae647fc65431c4839f1798522c
Dana <craciundaniela19@gmail.com>
2018-01-22 20:40:54 +0200

    Added integrated docs for APIs

--- deliveryh-service/urls.py
+++ deliveryh-service/urls.py
@@ -3,4 +3,5 @@ from rest_framework.documentation import include_docs_urls
 
 urlpatterns = [
     url(r'^', include('restaurants.urls')),
+    url(r'^docs/', include_docs_urls(title='Restaurant API documentation')),
 ]
